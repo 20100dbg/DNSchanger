4bb5c13eb2a5de24572d2e8bde59318bcd94e47b
20100dbg <vincent.marie80@gmail.com>
2020-02-23 20:44:34 +0100

    Replaced netsh scripts by powershell. Netsh scripts are in source code, but not used

--- DNSchanger/DNSchanger.Designer.cs
+++ DNSchanger/DNSchanger.Designer.cs
@@ -37,7 +37,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.cb_interfaces = new System.Windows.Forms.ComboBox();
             this.link = new System.Windows.Forms.LinkLabel();
-            this.l_description = new System.Windows.Forms.TextBox();
             this.tb_log = new System.Windows.Forms.TextBox();
             this.tb_newSec = new System.Windows.Forms.TextBox();
             this.tb_newPri = new System.Windows.Forms.TextBox();
@@ -45,11 +44,15 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.b_saveAuto = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.l_ip = new System.Windows.Forms.Label();
+            this.cb_validate = new System.Windows.Forms.CheckBox();
+            this.l_description = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tb_curPri
             // 
-            this.tb_curPri.Location = new System.Drawing.Point(91, 66);
+            this.tb_curPri.Location = new System.Drawing.Point(91, 90);
             this.tb_curPri.Name = "tb_curPri";
             this.tb_curPri.Size = new System.Drawing.Size(108, 20);
             this.tb_curPri.TabIndex = 0;
@@ -57,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 69);
+            this.label1.Location = new System.Drawing.Point(21, 93);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 1;
@@ -66,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 107);
+            this.label2.Location = new System.Drawing.Point(4, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 13);
             this.label2.TabIndex = 3;
@@ -74,7 +77,7 @@
             // 
             // tb_curSec
             // 
-            this.tb_curSec.Location = new System.Drawing.Point(91, 104);
+            this.tb_curSec.Location = new System.Drawing.Point(91, 128);
             this.tb_curSec.Name = "tb_curSec";
             this.tb_curSec.Size = new System.Drawing.Size(108, 20);
             this.tb_curSec.TabIndex = 2;
@@ -85,15 +88,15 @@
             this.cb_predefinedDNS.FormattingEnabled = true;
             this.cb_predefinedDNS.Location = new System.Drawing.Point(462, 12);
             this.cb_predefinedDNS.Name = "cb_predefinedDNS";
-            this.cb_predefinedDNS.Size = new System.Drawing.Size(111, 21);
+            this.cb_predefinedDNS.Size = new System.Drawing.Size(162, 21);
             this.cb_predefinedDNS.TabIndex = 4;
             this.cb_predefinedDNS.SelectedIndexChanged += new System.EventHandler(this.cb_predefinedDNS_SelectedIndexChanged);
             // 
             // b_save
             // 
-            this.b_save.Location = new System.Drawing.Point(268, 149);
+            this.b_save.Location = new System.Drawing.Point(235, 186);
             this.b_save.Name = "b_save";
-            this.b_save.Size = new System.Drawing.Size(75, 23);
+            this.b_save.Size = new System.Drawing.Size(108, 23);
             this.b_save.TabIndex = 5;
             this.b_save.Text = "Apply";
             this.b_save.UseVisualStyleBackColor = true;
@@ -121,41 +124,32 @@
             // link
             // 
             this.link.AutoSize = true;
-            this.link.Location = new System.Drawing.Point(474, 159);
+            this.link.Location = new System.Drawing.Point(525, 158);
             this.link.Name = "link";
             this.link.Size = new System.Drawing.Size(99, 13);
             this.link.TabIndex = 9;
             this.link.TabStop = true;
             this.link.Text = "Moar DNS services";
             // 
-            // l_description
-            // 
-            this.l_description.Location = new System.Drawing.Point(383, 66);
-            this.l_description.Multiline = true;
-            this.l_description.Name = "l_description";
-            this.l_description.ReadOnly = true;
-            this.l_description.Size = new System.Drawing.Size(190, 57);
-            this.l_description.TabIndex = 10;
-            // 
             // tb_log
             // 
-            this.tb_log.Location = new System.Drawing.Point(91, 201);
+            this.tb_log.Location = new System.Drawing.Point(91, 215);
             this.tb_log.Multiline = true;
             this.tb_log.Name = "tb_log";
             this.tb_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tb_log.Size = new System.Drawing.Size(482, 50);
+            this.tb_log.Size = new System.Drawing.Size(533, 50);
             this.tb_log.TabIndex = 11;
             // 
             // tb_newSec
             // 
-            this.tb_newSec.Location = new System.Drawing.Point(235, 104);
+            this.tb_newSec.Location = new System.Drawing.Point(235, 128);
             this.tb_newSec.Name = "tb_newSec";
             this.tb_newSec.Size = new System.Drawing.Size(108, 20);
             this.tb_newSec.TabIndex = 13;
             // 
             // tb_newPri
             // 
-            this.tb_newPri.Location = new System.Drawing.Point(235, 66);
+            this.tb_newPri.Location = new System.Drawing.Point(235, 90);
             this.tb_newPri.Name = "tb_newPri";
             this.tb_newPri.Size = new System.Drawing.Size(108, 20);
             this.tb_newPri.TabIndex = 12;
@@ -163,7 +157,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(125, 50);
+            this.label4.Location = new System.Drawing.Point(125, 74);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 13);
             this.label4.TabIndex = 14;
@@ -172,7 +166,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(270, 50);
+            this.label5.Location = new System.Drawing.Point(270, 74);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 15;
@@ -189,19 +183,62 @@
             // 
             // b_saveAuto
             // 
-            this.b_saveAuto.Location = new System.Drawing.Point(91, 149);
+            this.b_saveAuto.Location = new System.Drawing.Point(91, 186);
             this.b_saveAuto.Name = "b_saveAuto";
-            this.b_saveAuto.Size = new System.Drawing.Size(117, 23);
+            this.b_saveAuto.Size = new System.Drawing.Size(108, 23);
             this.b_saveAuto.TabIndex = 17;
             this.b_saveAuto.Text = "Set default (DHCP)";
             this.b_saveAuto.UseVisualStyleBackColor = true;
             this.b_saveAuto.Click += new System.EventHandler(this.b_saveAuto_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(66, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "IP";
+            // 
+            // l_ip
+            // 
+            this.l_ip.AutoSize = true;
+            this.l_ip.Location = new System.Drawing.Point(88, 41);
+            this.l_ip.Name = "l_ip";
+            this.l_ip.Size = new System.Drawing.Size(0, 13);
+            this.l_ip.TabIndex = 19;
+            // 
+            // cb_validate
+            // 
+            this.cb_validate.AutoSize = true;
+            this.cb_validate.Checked = true;
+            this.cb_validate.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_validate.Location = new System.Drawing.Point(235, 154);
+            this.cb_validate.Name = "cb_validate";
+            this.cb_validate.Size = new System.Drawing.Size(64, 17);
+            this.cb_validate.TabIndex = 20;
+            this.cb_validate.Text = "Validate";
+            this.cb_validate.UseVisualStyleBackColor = true;
+            // 
+            // l_description
+            // 
+            this.l_description.Location = new System.Drawing.Point(387, 42);
+            this.l_description.Multiline = true;
+            this.l_description.Name = "l_description";
+            this.l_description.ReadOnly = true;
+            this.l_description.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.l_description.Size = new System.Drawing.Size(237, 106);
+            this.l_description.TabIndex = 21;
+            // 
             // DNSchanger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 269);
+            this.ClientSize = new System.Drawing.Size(636, 274);
+            this.Controls.Add(this.l_description);
+            this.Controls.Add(this.cb_validate);
+            this.Controls.Add(this.l_ip);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.b_saveAuto);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -209,7 +246,6 @@
             this.Controls.Add(this.tb_newSec);
             this.Controls.Add(this.tb_newPri);
             this.Controls.Add(this.tb_log);
-            this.Controls.Add(this.l_description);
             this.Controls.Add(this.link);
             this.Controls.Add(this.cb_interfaces);
             this.Controls.Add(this.label3);
@@ -238,7 +274,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox cb_interfaces;
         private System.Windows.Forms.LinkLabel link;
-        private System.Windows.Forms.TextBox l_description;
         private System.Windows.Forms.TextBox tb_log;
         private System.Windows.Forms.TextBox tb_newSec;
         private System.Windows.Forms.TextBox tb_newPri;
@@ -246,6 +281,10 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button b_saveAuto;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label l_ip;
+        private System.Windows.Forms.CheckBox cb_validate;
+        private System.Windows.Forms.TextBox l_description;
     }
 }
 
--- DNSchanger/DNSchanger.cs
+++ DNSchanger/DNSchanger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Management.Automation;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Windows.Forms;
@@ -13,81 +14,82 @@ namespace DNSchanger
     {
         List<ServiceDns> listDns = new List<ServiceDns>();
         List<Interface> listInterface = new List<Interface>();
+        enum Method { Netsh1, Netsh2, Powershell };
+
+
+        //netsh interface ip set dns name="Local Area Connection" static 208.67.222.222
+        //netsh interface ip add dns name="Local Area Connection" 208.67.220.220 index=2
+        //netsh interface ip set dnsservers name="Local Area Connection" source=dhcp
+        //netsh interface ip set dns « Description » static %DNS%
+
+        //Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses ("10.0.0.1","10.0.0.2")
+        //Set-DnsClientServerAddress -InterfaceIndex 12 -ResetServerAddresses
 
         public DNSchanger()
         {
             InitializeComponent();
 
+            InitInterface();
+        }
+
+        private void InitInterface()
+        {
             LinkLabel.Link l = new LinkLabel.Link();
             l.LinkData = "https://wikileaks.org/wiki/Alternative_DNS";
             link.Links.Add(l);
-            
-
 
             //Liste les interfaces
+
+            /*
             foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
                 listInterface.Add(new Interface { Id = nic.Id, Name = nic.Name });
+            */
 
+            ListInterfaces();
             cb_interfaces.Items.AddRange(listInterface.ToArray());
 
 
             //Charge et liste les services DNS
             StreamReader sr = new StreamReader("dns.csv");
-            String line;
-            Boolean headers = false;
+            sr.ReadLine(); //Ignore les headers
 
+            String line;
             while ((line = sr.ReadLine()) != null)
             {
-                if (!headers)
-                {
-                    headers = true;
-                    line = sr.ReadLine();
-                }
-
                 String[] tab = line.Split(';');
 
-                listDns.Add(new ServiceDns { Name = tab[0],
-                                            PrimaryIP = IPAddress.Parse(tab[1]),
-                                            SecondaryIP = IPAddress.Parse(tab[2]),
-                                            Description = tab[3] });
+                listDns.Add(new ServiceDns
+                {
+                    Name = tab[0],
+                    PrimaryIP = IPAddress.Parse(tab[1]),
+                    SecondaryIP = IPAddress.Parse(tab[2]),
+                    Description = tab[3].Replace("- ", Environment.NewLine + "- ")
+                });
             }
-            
+
             cb_predefinedDNS.Items.AddRange(listDns.ToArray());
+
         }
 
 
-        private Boolean checkIp(String strIP)
+        private void ListInterfaces()
         {
-            IPAddress ip;
-            return IPAddress.TryParse(strIP, out ip);
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Get-NetAdapter");
+            var result = ps.Invoke();
+
+            foreach (var pso in result)
+            {
+                listInterface.Add(new Interface { InterfaceIndex = Convert.ToUInt32(pso.Members["InterfaceIndex"].Value),
+                                                    Name = pso.Members["Name"].Value.ToString(),
+                                                    Id = pso.Members["InterfaceGuid"].Value.ToString(),
+                });
+            }
+
         }
 
-        //Set to auto
-        private void ApplyDns(String description)
-        {
-            String cmd = "netsh interface ip set dns \"{0}\" dhcp";
-            cmd = String.Format(cmd, description);
-            String result = startCmd(cmd);
-            result = result.Trim();
 
-            if (String.IsNullOrWhiteSpace(result)) tb_log.AppendText("DNS has been set to auto");
-            else tb_log.AppendText(result);
-            tb_log.AppendText(Environment.NewLine);
-        }
-
-        private void ApplyDns(String description, String ip, int index)
-        {
-            String cmd = "netsh interface ipv4 add dnsserver \"{0}\" address={1} index={2}";
-            cmd = String.Format(cmd, description, ip, index);
-            String result = startCmd(cmd);
-            result = result.Trim();
-
-            if (String.IsNullOrWhiteSpace(result)) tb_log.AppendText("DNS has been updated");
-            else tb_log.AppendText(result);
-            tb_log.AppendText(Environment.NewLine);
-        }
-
-        private void ResetDns(String description)
+        private void NshResetDns(String description)
         {
             String cmd = "netsh interface ipv4 delete dnsservers \"{0}\" all";
             cmd = String.Format(cmd, description);
@@ -95,7 +97,92 @@ namespace DNSchanger
             result = result.Trim();
 
             if (String.IsNullOrWhiteSpace(result))
-                tb_log.AppendText("DNS has been reset" + Environment.NewLine);
+                Log("DNS has been reset");
+        }
+
+
+        private void NshDefaultDns(String description)
+        {
+            String cmd = "netsh interface ip set dns \"{0}\" dhcp";
+            cmd = String.Format(cmd, description);
+            String result = startCmd(cmd);
+            result = result.Trim();
+
+            if (String.IsNullOrWhiteSpace(result)) Log("DNS has been set to auto");
+            else Log(result);
+        }
+
+
+        private void NshApplyDns(String description, String ip, int index, Method m)
+        {
+            String cmd = "";
+
+            if (m == Method.Netsh1)
+                cmd = "netsh interface ipv4 add dnsserver \"{0}\" address={1} index={2}";
+            else if (m == Method.Netsh2)
+                cmd = "netsh interface ip set dns name=\"{0}\" {1} index={2}";
+
+            cmd = String.Format(cmd, description, ip, index);
+            String result = startCmd(cmd);
+            result = result.Trim();
+
+            if (String.IsNullOrWhiteSpace(result)) Log("DNS has been updated");
+            else Log(result);
+        }
+
+
+
+        private void PsDefaultDns(int idxInterface)
+        {
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Set-DnsClientServerAddress");
+            ps.AddParameter("InterfaceIndex", idxInterface);
+            ps.AddParameter("ResetServerAddresses");
+            ps.Invoke();
+        }
+
+        private void PsApplyDns(UInt32 idxInterface, String[] ip, Boolean validate = true)
+        {
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Set-DnsClientServerAddress");
+            ps.AddParameter("InterfaceIndex", idxInterface);
+            ps.AddParameter("ServerAddresses", ip);
+
+            if (validate)
+                ps.AddParameter("Validate");
+
+            ps.Invoke();
+        }
+
+
+        private Boolean CheckAppliedDns(Interface iface, String ip)
+        {
+            Boolean flag = false;
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.Id == iface.Id)
+                {
+                    IPAddressCollection ipList = nic.GetIPProperties().DnsAddresses;
+
+                    for (int i = 0; i < ipList.Count; i++)
+                        if (ip == ipList[i].ToString()) flag = true;
+                }
+            }
+
+            return flag;
+        }
+
+
+        private void Log(String txt)
+        {
+            tb_log.AppendText(txt + Environment.NewLine);
+        }
+
+        private Boolean checkIp(String strIP)
+        {
+            IPAddress ip;
+            return IPAddress.TryParse(strIP, out ip);
         }
 
         private String startCmd(String cmd)
@@ -122,7 +209,7 @@ namespace DNSchanger
             {
                 tb_newPri.Text = listDns[idx].PrimaryIP.ToString();
                 tb_newSec.Text = listDns[idx].SecondaryIP.ToString();
-                l_description.Text = listDns[idx].Description;
+                l_description.Text = listDns[idx].Description.Trim(new char[] { '"' });
             }
         }
 
@@ -138,6 +225,14 @@ namespace DNSchanger
                 {
                     if (nic.Id == id)
                     {
+                        UnicastIPAddressInformationCollection ips = nic.GetIPProperties().UnicastAddresses;
+
+                        for (int i = 0; i< ips.Count; i++)
+                        {
+                            if (ips[i].IPv4Mask != null)
+                                l_ip.Text = ips[i].Address.ToString();
+                        }
+                        
                         IPAddressCollection ipList = nic.GetIPProperties().DnsAddresses;
 
                         if (ipList.Count > 0) tb_curPri.Text = ipList[0].ToString();
@@ -154,22 +249,28 @@ namespace DNSchanger
             int idx = cb_interfaces.SelectedIndex;
             if (idx > -1)
             {
-                String description = listInterface[idx].Name;
-
+                List<String> ips = new List<String>();
+                
                 if (!String.IsNullOrWhiteSpace(tb_newPri.Text) && checkIp(tb_newPri.Text))
+                    ips.Add(tb_newPri.Text);
+
+                if (!String.IsNullOrWhiteSpace(tb_newSec.Text) && checkIp(tb_newSec.Text))
+                    ips.Add(tb_newSec.Text);
+
+                if (ips.Count > 0)
                 {
-                    ResetDns(description);
+                    PsApplyDns(listInterface[idx].InterfaceIndex, ips.ToArray(), true);
 
-                    ApplyDns(description, tb_newPri.Text, 1);
+                    if (CheckAppliedDns(listInterface[idx], ips[0]))
+                        Log("Successfully set Primary DNS : " + ips[0]);
+                    else
+                        Log("Seems I couldn't set Primary DNS : " + ips[0]);
 
-                    if (!String.IsNullOrWhiteSpace(tb_newSec.Text) && checkIp(tb_newSec.Text))
-                    {
-                        ApplyDns(description, tb_newSec.Text, 2);
-                    }
+                    if (ips.Count > 1 && CheckAppliedDns(listInterface[idx], ips[1]))
+                        Log("Successfully set Secondary DNS : " + ips[1]);
+                    else
+                        Log("Seems I couldn't set Secondary DNS : " + ips[1]);
                 }
-                else
-                    ApplyDns(description);
-
             }
         }
 
@@ -178,9 +279,8 @@ namespace DNSchanger
             int idx = cb_interfaces.SelectedIndex;
             if (idx > -1)
             {
-                String description = listInterface[idx].Name;
-                ResetDns(description);
-                ApplyDns(description);
+                PsDefaultDns(idx);
+                Log("Set DNS to DHCP default");
             }
                 
         }
@@ -188,6 +288,10 @@ namespace DNSchanger
 
     class Interface
     {
+        public UInt32 InterfaceIndex { get; set; }
+
+
+
         public String Id { get; set; }
         public String Name { get; set; }
 
